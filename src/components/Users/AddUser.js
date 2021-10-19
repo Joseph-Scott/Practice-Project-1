de55b5372f19f970e05126331be396919b6448a4
Joe Scott <jbs198585@gmail.com>
2021-10-19 06:29:32 -0400

    JSX limitations and workarounds

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -44,14 +44,14 @@ const AddUser = props => {
   };
 
   return (
-    [
-      error && (
+    <div>
+      {error && (
         <ErrorModal 
           title={error.title} 
           message={error.message} 
           onConfirm={errorHandler} 
-      />
-      ),
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -71,7 +71,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    ]
+    </div>
   );
 };
 
